7ae455448e145e98469f5156b7b298832ab83230
watanabe <ce66034@meiji.ac.jp>
2017-05-21 23:46:37 +0900

    [init]

---
+++ Makefile
@@ -0,0 +1,21 @@
+CXX = g++
+
+PROG := main.out
+SRCS := main.o
+SRCS += eh_frame.o dwarf4.o
+#SRCS += ia64_cxx_abi.o
+#SRCS += registers_intel_x64.o
+OBJS := $(SRCS:%.c=%.o)
+
+CXXFLAGS := -g -Wall -gdwarf-4 -std=gnu++14
+
+all: $(PROG)
+$(PROG): $(OBJS)
+	$(CXX) $^ -o $@
+
+%.o: %.c
+	$(CXX) $(CXXFLAGS) $(SRCS) -c
+
+clean:
+	rm -f $(PROG) $(OBJS)
+
---
+++ main.cpp
@@ -0,0 +1,101 @@
+#include <link.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include "eh_frame_list.h"
+
+#include "eh_frame.h"
+
+uintptr_t g_offs = 0;
+uintptr_t g_size = 0;
+
+static int
+callback(struct dl_phdr_info *info, size_t size, void *data)
+{
+    (void) size;
+    (void) data;
+    static auto once = false;
+
+    if (once) return 0;
+    once = true;
+
+    for (int i = 0; i < info->dlpi_phnum; i++)
+    {
+        if (info->dlpi_phdr[i].p_type == PT_LOAD)
+        {
+            g_offs = info->dlpi_addr;
+            break;
+        }
+    }
+
+    return 0;
+}
+
+eh_frame_t g_eh_frame_list[100] = {{nullptr, 0}};
+
+extern "C" struct eh_frame_t *get_eh_frame_list() noexcept
+{
+	g_eh_frame_list[0].addr = reinterpret_cast<void *>(g_offs);
+	g_eh_frame_list[0].size = g_size;
+
+	return static_cast<struct eh_frame_t *>(g_eh_frame_list);
+}
+
+int main()
+{
+	dl_iterate_phdr(callback, nullptr);
+
+	std::stringstream eh_frame_offs_ss;
+	std::stringstream eh_frame_size_ss;
+
+	eh_frame_offs_ss << "readelf -SW /proc/" << getpid() << "/exe | grep \".eh_frame\" | grep -v \".eh_frame_hdr\" | awk '{print $4}' > offs.txt";
+	eh_frame_size_ss << "readelf -SW /proc/" << getpid() << "/exe | grep \".eh_frame\" | grep -v \".eh_frame_hdr\" | awk '{print $6}' > size.txt";
+
+	system(eh_frame_offs_ss.str().c_str());
+	system(eh_frame_size_ss.str().c_str());
+
+	auto &&offs_file = std::ifstream("offs.txt");
+	auto &&size_file = std::ifstream("size.txt");
+
+	auto &&offs_str = std::string((std::istreambuf_iterator<char>(offs_file)), std::istreambuf_iterator<char>());
+	auto &&size_str = std::string((std::istreambuf_iterator<char>(size_file)), std::istreambuf_iterator<char>());
+
+	/* file value */
+//	std::cout << offs_str;
+//	std::cout << size_str;
+
+	std::remove("offs.txt");
+	std::remove("size.txt");
+
+	g_offs += std::stoull(offs_str, nullptr, 16);
+	g_size += std::stoull(size_str, nullptr, 16);
+
+//	std::cout << std::hex << g_offs << std::endl;
+//	std::cout << std::hex << g_size << std::endl;
+
+	struct eh_frame_t *eh_frame = get_eh_frame_list();
+	debug("eh_frame.addr: %p\n", eh_frame->addr);
+	debug("eh_frame.size: 0x%lx\n", eh_frame->size);
+
+	// ci_entry
+	ci_entry *ci = new ci_entry(*eh_frame);
+
+	uint64_t pointer_encoding = ci->m_pointer_encoding;
+	char *instructions = ci->initial_instructions();
+	ci->dump();
+
+	uint64_t dp = decode_pointer(&instructions, pointer_encoding);
+	debug("decode_pointer: 0x%lx\n", dp);
+
+
+	// fd_entry
+	fd_entry *fd = new fd_entry(*eh_frame);
+	fd->dump();
+
+	return 0;
+}
+
